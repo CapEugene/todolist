683ef9aaee091e5b8d811ee4fb3607d2625e191d
CapEugene <evgexa_2003@bk.ru>
2025-02-19 19:20:21 +0300

    First version of working ToDoList

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -1,12 +1,21 @@
 import './navbar.css';
 
 function Navbar() {
+
+    function exit() {
+        alert("Прост выход :)");
+    }
+
+    function profile() {
+        alert("Прост профиль :)");
+    }
+
     return (
         <nav className='navbar'>
             <h1>ToDoList</h1>
             <div className='buttons'>
-                <button className='button'>Exit</button>
-                <button className='button'>Profile</button>
+                <button className='button' onClick={exit}>Exit</button>
+                <button className='button' onClick={profile}>Profile</button>
             </div>
         </nav>
     );
--- src/todolist/todolist.jsx
+++ src/todolist/todolist.jsx
@@ -1,10 +1,48 @@
+import { useState, useEffect } from 'react';
 import Navbar from '../navbar/navbar';
 import './todolist.css';
 import Task from '../task/task';
 
 function MainPage() {
 
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        getTasks();
+    }, []);
     
+    const getTasks = () => {
+        const retrievedTasks = [];
+        for (let i = 1; i <= localStorage.length; i++) {
+            const retrievedTask = JSON.parse(localStorage.getItem(i));
+            retrievedTasks.push(retrievedTask.taskinfo);
+        }
+        setTasks(retrievedTasks);
+    };
+
+    const addTask = () => {
+        if (document.getElementById("input").value == '') {
+            alert("Строка пуста!");
+        }
+        else {
+            const input = document.getElementById("input").value;
+
+            const task = {
+                id: localStorage.length + 1,
+                taskinfo: input,
+            };
+
+            localStorage.setItem(task.id, JSON.stringify(task));
+            setTasks([...tasks, input]);
+            document.getElementById("input").value = '';
+        }
+    };
+
+    const clearLocalStorage = () => {
+        localStorage.clear();
+        getTasks();
+        alert("Выполнено!");
+    };
 
     return (
         <>
@@ -13,14 +51,17 @@ function MainPage() {
 
                 <div className='task-create'>
                     <div className='buttons'>
-                        <input></input>
-                        <button>Add new task</button>
+                        <input id="input"></input>
+                        <button onClick={addTask}>Add new task</button>
+                        <button onClick={clearLocalStorage}>Clear localStorage</button>
                     </div>
                 </div>
 
-                <div className='tasks-container'>
-
-                </div>
+                <ul className='tasks-container'>
+                    {tasks.map((task, index) => (
+                        <li className='list-container' key={index}>{task}</li>
+                    ))}
+                </ul>
             </div>
         </>
     );
